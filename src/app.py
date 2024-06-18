2489385f2ceeff8ecc535276f42754886164bf63
DESKTOP-5GTG24U <j@fast.ai>
2024-06-18 14:40:52 -0600

    add api key

--- src/app.py
+++ src/app.py
@@ -1,8 +1,28 @@
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
 
 def get_response(user_input):
     return "I don't know"
+
+def get_vector_store(url):
+    """
+    Get text in document form
+    """
+    loader = WebBaseLoader(url)
+    document = loader.load()
+
+    # split document into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+
+    # creatre vector store
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+      
+    return vector_store
 
 # app config
 st.set_page_config(page_title = "Chat with websites!",page_icon ="🤖")
@@ -19,6 +39,8 @@ with st.sidebar:
 if website_url is None or website_url == "":
     st.info("Please enter a website URL")
 else:
+    document_chunks = get_vector_store(website_url)
+    
     # user input
     user_query = st.chat_input("Type your message here...")
     if user_query is not None and user_query != "":
